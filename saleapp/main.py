20fcd820c7b49084fb87ff06cd44435b863aab4f
Hwi WonWoo <huynguyenvu.it@gmail.com>
2020-08-17 10:48:52 +0700

    them 17

--- saleapp/main.py
+++ saleapp/main.py
@@ -1,6 +1,7 @@
-from flask import render_template, request
-from saleapp import app
-from saleapp import dao
+from flask import render_template, request, redirect, url_for, jsonify, send_file, session
+from saleapp import app, dao, utils
+from saleapp import decorator
+import json
 
 
 @app.route("/")
@@ -24,5 +25,134 @@ def product_list_by_cate(category_id):
     return render_template("product-list.html", products=dao.read_products_by_cate_id(cate_id=category_id))
 
 
+@app.route("/api/products/<int:product_id>", methods=["delete"])
+def delete_product(product_id):
+    if dao.delete_product(product_id=product_id):
+        return jsonify({"status": 200, "product_id": product_id})
+
+    return jsonify({"status": 500, "error_message": "Something Wrong!!!"})
+
+
+@app.route("/products/add", methods=["get", "post"])
+@decorator.login_required
+def add_product():
+    """
+    add: /products/add
+    update: /products/add?product_id
+    :return: template
+    """
+    err_msg = None
+    if request.method.lower() == "post":
+        if request.args.get("product_id"): # UPDATE
+            d = dict(request.form.copy())
+            d["product_id"] = request.args["product_id"]
+            if dao.update_product(**d):
+                return redirect(url_for('product_list', product_id=d["product_id"]))
+            else:
+                err_msg = "Something wrong, go back later!!!"
+        else: # ADD
+            if dao.add_product(**dict(request.form)):
+                return redirect(url_for('product_list'))
+            else:
+                err_msg = "Something wrong, go back later!!!"
+
+    categories = dao.read_categories()
+    product = None
+    if request.args.get("product_id"):
+        product = dao.read_product_by_id(product_id=int(request.args["product_id"]))
+
+    return render_template("product-add.html", categories=categories, product=product, err_msg=err_msg)
+
+
+@app.route("/products/export")
+def export_product():
+    p = utils.export()
+
+    return send_file(filename_or_fp=p)
+
+
+@app.route("/api/pro/<int:product_id>", methods=["delete"])
+def del_pro(product_id):
+    if dao.del_product(product_id=product_id):
+        return jsonify({"status": 200, "product_id": product_id})
+
+    return jsonify({"status": 500, "err_msg": "Something Wrong!!!"})
+
+
+@app.route("/login", methods=["get", "post"])
+def login():
+    err_msg = ""
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = dao.check_login(username=username, password=password)
+        if user:
+            # Login thanh cong
+            session["user"] = user
+            if "next" in request.args:
+                return redirect(request.args["next"])
+
+            return redirect(url_for('index'))
+        else:
+            err_msg = "Something wrong!!!"
+
+    return render_template("login.html", err_msg=err_msg)
+
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        session["user"] = None
+
+    return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["get", "post"])
+def register():
+    if session.get("user"):
+        return redirect(request.url)
+
+    err_msg = ""
+    if request.method == "POST":
+        name = request.form.get("name")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+        if password.strip() != confirm.strip():
+            err_msg = "Mat khau khong khop"
+        else:
+            if dao.add_user(name=name, username=username, password=password):
+                return redirect(url_for("login"))
+            else:
+                err_msg = "Something Wrong!!!"
+
+    return render_template("register.html", err_msg=err_msg)
+
+
+
+
+@app.route("/api/cart", methods=["post"])
+def cart():
+    data = json.loads(request.data)
+    product_id = data.get("product_id")
+    name = data.get("name")
+    price = data.get("price")
+    if "cart" not in session:
+        session["cart"] = {}
+    cart = session["cart"]
+    product_key = str(product_id)
+    if product_key in cart:
+        cart[product_key]["quantity"] = cart[product_key]["quantity"] + 1
+    else:
+        cart[product_key] = {
+            "id": product_id,
+            "name": name,
+            "price": price,
+            "quantity": 1
+        }
+
+    session["cart"] = cart
+    return jsonify({"success": 1, "quantity": sum([c["quantity"] for c in list(session["cart"].values())])})
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5050)
